4a33145a61dbd7c3e1f65240afb0fdc96bc8c3df
nineInchClous <thibault.castelli@pm.me>
2023-08-09 11:06:31 -0400

    Add playRound func:
    Play a round and declare the winner using two parameters, the player choice (string)
    and the computer choice (string).

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function getComputerChoice() {
   const choiceIndex = Math.floor(Math.random() * choices.length);
   return choices[choiceIndex];
 }
+
+/**
+ * Play a round and declare the winner
+ * @param {string} playerChoice Player choice this round ('Rock', 'Paper', or 'Scissors')
+ * @param {string} computerChoice Computer choice this round ('Rock', 'Paper', or 'Scissors')
+ * @returns {string} A string that declares the winner of round
+ */
+function playRound(playerChoice, computerChoice) {
+  // Make the function case-insensitive
+  playerChoice = playerChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
+
+  // Tie
+  if (playerChoice === computerChoice) {
+    return 'That\' a tie!';
+  }
+  // Player wins
+  else if (playerChoice === 'ROCK' && computerChoice === 'SCISSORS' ||
+    playerChoice === 'PAPER' && computerChoice === 'ROCK' ||
+    playerChoice === 'SCISSORS' && computerChoice === 'PAPER') {
+    return `You win! ${playerChoice} beats ${computerChoice}.`
+  }
+  // Computer wins
+  return `You loose! ${computerChoice} beats ${playerChoice}`;
+}
