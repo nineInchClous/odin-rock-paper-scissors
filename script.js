4fe83dc6a3d96ed4cbfb0aca4505619eefd2fb1e
nineInchClous <thibault.castelli@pm.me>
2023-08-09 11:29:22 -0400

    Add game func:
    Play 5 rounds of rock, paper, scissors, asking for the player to
    enter his choice.
    Also add helper function validPlayerChoice to check if the choice of
    the player is valid.
    Add addScore function to increase the score of the player or the computer based on the
    round result.

--- script.js
+++ script.js
@@ -32,3 +32,72 @@ function playRound(playerChoice, computerChoice) {
   // Computer wins
   return `You loose! ${computerChoice} beats ${playerChoice}`;
 }
+
+/**
+ * Check if the player's choice is a valid choice ('Rock', 'Paper', or 'Scissors')
+ * @param {string} playerChoice Player choice this round ('Rock', 'Paper', or 'Scissors')
+ * @returns {boolean} True if the player choice is either 'Rock', 'Paper', or 'Scissors', otherwise return false
+ */
+function validPlayerChoice(playerChoice) {
+  if (playerChoice === null) {
+    return false;
+  }
+
+  // Make the function case-insensitive
+  playerChoice = playerChoice.toUpperCase();
+
+  return playerChoice === 'ROCK' || playerChoice === 'PAPER' || playerChoice === 'SCISSORS';
+}
+
+/**
+ * Add score for the player or the computer based on the round result
+ * @param {string} roundResult Result of the round
+ * @param {object} scores Scores object with player and computer score as properties
+ */
+function addScore(roundResult, scores) {
+  if (roundResult.includes('win')) {
+    scores.player++;
+  }
+  else if (roundResult.includes('loose')) {
+    scores.computer++;
+  }
+}
+
+/**
+ * Play a full game (5 rounds) of rock, paper, scissors
+ */
+function game() {
+  // Initialize variables
+  const scores = { player: 0, computer: 0 };
+  let playerChoice = '';
+  let computerChoice = '';
+  let roundResult = '';
+
+  // Play 5 rounds
+  for (let i = 0; i < 5; i++) {
+    // Make sure the player enters a valid choice
+    while (!validPlayerChoice(playerChoice)) {
+      playerChoice = prompt('Enter "Rock", "Paper", or "Scissors" to play: ');
+
+      // Log error message if the choice is not valid
+      if (!validPlayerChoice(playerChoice)) {
+        console.log('Wrong value, please enter "Rock", "Paper", or "Scissors".');
+      }
+    }
+
+    // Play the round
+    computerChoice = getComputerChoice();
+    roundResult = playRound(playerChoice, computerChoice);
+    addScore(roundResult, scores);
+
+    // Log choices
+    console.log(`You chose ${playerChoice}.\nComputer chose ${computerChoice}.`);
+
+    // Log round result and scores
+    console.log(roundResult);
+    console.log(`Your score: ${scores.player} | Computer's score: ${scores.computer}`);
+
+    // Reset playerChoice
+    playerChoice = '';
+  }
+}
