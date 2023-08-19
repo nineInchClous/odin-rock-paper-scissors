cf78b59cda5001a2f0818896fe2957da2a462f9d
nineInchClous <thibault.castelli@pm.me>
2023-08-19 10:54:23 -0400

    Create buttons for playing:
    Add an event listener on each of the button, playing a round.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll('#btns-sect button');
+buttons[0].addEventListener('click', () => { playRound('rock', getComputerChoice()) });
+buttons[1].addEventListener('click', () => { playRound('paper', getComputerChoice()) });
+buttons[2].addEventListener('click', () => { playRound('scissors', getComputerChoice()) });
+
 /**
  * Randomly return 'Rock', 'Paper' or 'Scissors'.
  * @returns 'Rock', 'Paper' or 'Scissors' as a string
@@ -33,21 +38,21 @@ function playRound(playerChoice, computerChoice) {
   return `You loose! ${computerChoice} beats ${playerChoice}`;
 }
 
-/**
- * Check if the player's choice is a valid choice ('Rock', 'Paper', or 'Scissors')
- * @param {string} playerChoice Player choice this round ('Rock', 'Paper', or 'Scissors')
- * @returns {boolean} True if the player choice is either 'Rock', 'Paper', or 'Scissors', otherwise return false
- */
-function validPlayerChoice(playerChoice) {
-  if (playerChoice === null) {
-    return false;
-  }
+// /**
+//  * Check if the player's choice is a valid choice ('Rock', 'Paper', or 'Scissors')
+//  * @param {string} playerChoice Player choice this round ('Rock', 'Paper', or 'Scissors')
+//  * @returns {boolean} True if the player choice is either 'Rock', 'Paper', or 'Scissors', otherwise return false
+//  */
+// function validPlayerChoice(playerChoice) {
+//   if (playerChoice === null) {
+//     return false;
+//   }
 
-  // Make the function case-insensitive
-  playerChoice = playerChoice.toUpperCase();
+//   // Make the function case-insensitive
+//   playerChoice = playerChoice.toUpperCase();
 
-  return playerChoice === 'ROCK' || playerChoice === 'PAPER' || playerChoice === 'SCISSORS';
-}
+//   return playerChoice === 'ROCK' || playerChoice === 'PAPER' || playerChoice === 'SCISSORS';
+// }
 
 /**
  * Add score for the player or the computer based on the round result
@@ -63,41 +68,41 @@ function addScore(roundResult, scores) {
   }
 }
 
-/**
- * Play a full game (5 rounds) of rock, paper, scissors
- */
-function game() {
-  // Initialize variables
-  const scores = { player: 0, computer: 0 };
-  let playerChoice = '';
-  let computerChoice = '';
-  let roundResult = '';
+// /**
+//  * Play a full game (5 rounds) of rock, paper, scissors
+//  */
+// function game() {
+//   // Initialize variables
+//   const scores = { player: 0, computer: 0 };
+//   let playerChoice = '';
+//   let computerChoice = '';
+//   let roundResult = '';
 
-  // Play 5 rounds
-  for (let i = 0; i < 5; i++) {
-    // Make sure the player enters a valid choice
-    while (!validPlayerChoice(playerChoice)) {
-      playerChoice = prompt('Enter "Rock", "Paper", or "Scissors" to play: ');
+//   // Play 5 rounds
+//   for (let i = 0; i < 5; i++) {
+//     // Make sure the player enters a valid choice
+//     while (!validPlayerChoice(playerChoice)) {
+//       playerChoice = prompt('Enter "Rock", "Paper", or "Scissors" to play: ');
 
-      // Log error message if the choice is not valid
-      if (!validPlayerChoice(playerChoice)) {
-        console.log('Wrong value, please enter "Rock", "Paper", or "Scissors".');
-      }
-    }
+//       // Log error message if the choice is not valid
+//       if (!validPlayerChoice(playerChoice)) {
+//         console.log('Wrong value, please enter "Rock", "Paper", or "Scissors".');
+//       }
+//     }
 
-    // Play the round
-    computerChoice = getComputerChoice();
-    roundResult = playRound(playerChoice, computerChoice);
-    addScore(roundResult, scores);
+//     // Play the round
+//     computerChoice = getComputerChoice();
+//     roundResult = playRound(playerChoice, computerChoice);
+//     addScore(roundResult, scores);
 
-    // Log choices
-    console.log(`You chose ${playerChoice}.\nComputer chose ${computerChoice}.`);
+//     // Log choices
+//     console.log(`You chose ${playerChoice}.\nComputer chose ${computerChoice}.`);
 
-    // Log round result and scores
-    console.log(roundResult);
-    console.log(`Your score: ${scores.player} | Computer's score: ${scores.computer}`);
+//     // Log round result and scores
+//     console.log(roundResult);
+//     console.log(`Your score: ${scores.player} | Computer's score: ${scores.computer}`);
 
-    // Reset playerChoice
-    playerChoice = '';
-  }
-}
+//     // Reset playerChoice
+//     playerChoice = '';
+//   }
+// }
